85e9593c5ecf8dd4a744f2b261b5e884706e57c7
keithdhd <keith@keithdouglas.co.uk>
2015-04-17 14:19:01 +0100

    finished

--- js/app.js
+++ js/app.js
@@ -4,13 +4,23 @@ $(document).ready(function(){
     theAccordion.accordion({ header: "h3", collapsible: true, active: false, fillSpace: true});
 
     numberOfQuestions = 0;
+    questionStack = new Array();
+    currentScore = 0;
 
     var answers1 = ["Nero", "Julius Caesar", "Augustus", "Marcus Aurelius"];
     var question1 = new Question("1. Who was the first Emperor of Rome?",answers1, 2);
   	
    	var answers2 = ["Cicero", "Julius Caesar", "Sparticus", "Antony"];
-    var question2 = new Question("2. Who was Augustus's adoptive uncle?",answers2, 2);
+    var question2 = new Question("2. Who was Augustus's adoptive uncle?",answers2, 1);
 
+   	var answers3 = ["True", "False"];
+    var question3 = new Question("3. Rome was a Republic before it became an Empire.",answers3, 0);
+
+   	var answers4 = ["Pompey", "Crassus", "Cato", "Scipio"];
+    var question4 = new Question("4. Julius Caesar crossed the Rubicon to drive out who from Rome?",answers4, 0);
+
+   	var answers5 = ["Battle of Queribus", "Battle of Actium", "Battle of Naples", "Battle of Alexandria"];
+    var question5 = new Question("5. Octavian annihilated Egyptian forces in which battle?",answers5, 1);
 
   	theAccordion.accordion( "refresh" );
 })
@@ -18,10 +28,12 @@ $(document).ready(function(){
 function Question(_questionText, _arrAnswers, _correctAnswer){
 	this.questionText = _questionText || "Question Text";
 	this.arrAnswers = _arrAnswers || new Array();
+	this.rightOrWrong;
 
  	this.init = function(){
 		createNewQuestionHTML(this.questionText, this.arrAnswers, _correctAnswer);
 		numberOfQuestions++;
+		questionStack[numberOfQuestions] = 0;
 	}
 
 	this.init();
@@ -31,45 +43,81 @@ function createNewQuestionHTML( _questionText, _arrAnswers, _correctAnswer){
 	
 		var answersHtmlString = "<h3>" + _questionText + "</h3>";
 
-		answersHtmlString += "<div><ul class='answerList'>";
+		var questionHeader = document.createElement("h3");
+		questionHeader.innerHTML = _questionText;
+		theAccordion.append(questionHeader);
 
-		$("#answerList").length;
+		var questionDiv = document.createElement("div");
+		questionDiv.id = "questionDiv" + numberOfQuestions;
+		theAccordion.append(questionDiv);
+
+		var ulElement = document.createElement("ul");
+		ulElement.id = "ulElement"+numberOfQuestions;
+		$("#questionDiv"+numberOfQuestions).append(ulElement);
 
 		for(i in _arrAnswers){
-			answersHtmlString += "<li><input type='radio' name='question" + numberOfQuestions + "' value=" + i + ">" + _arrAnswers[i] + "</li>";
+			$("#ulElement"+numberOfQuestions).append("<li><input type='radio' name='question" + numberOfQuestions + "' value=" + i + ">" + _arrAnswers[i] + "</li>");
 		}
 
-		answersHtmlString += "</ul>";
-
-		var theAnswerButton = new Button("Submit Answer","#587DB4", _correctAnswer).init();
-
-		answersHtmlString += theAnswerButton.outerHTML + "</div>";
-		theAccordion.append(answersHtmlString);
+		var theAnswerButton = new Button("Submit Answer","#587DB4", _correctAnswer, numberOfQuestions, _questionText).init();
+		$("#questionDiv"+numberOfQuestions).append(theAnswerButton);
 }
 
-function markAnswer(){
-	alert(this.correctAnswer);
+function markAnswer(_userAnswer, _correctAnswer, _qNumber, _questionText){
+	thisQuestionHeader = $("#ui-accordion-accordion-header-" + (_qNumber));
+
+	if(_userAnswer == _correctAnswer){
+		//Append a tick to question text
+		thisQuestionHeader.html(_questionText + " &#x2714;");
+		//Set header text to green
+		thisQuestionHeader.css("color","green");		
+		//Update currentScore. 
+		questionStack[_qNumber] = 1;
+
+	}
+	else{
+		//Append a cross to question text
+		thisQuestionHeader.html(_questionText +  " &#x2718;");
+		//Set header text to red
+		thisQuestionHeader.css("color","red");
+		questionStack[_qNumber] = 0;
+	}
+
+	currentScore = 0;
+	//Set current-score 
+	for(j in questionStack){
+		if(questionStack[j] == 1){
+			currentScore++;
+		}
+	}
+
+	//This doesn't work! And doesn't show an error. Why not?
+	//$("#current-score").text(currentScore);
+
+	$(".score").text("Score: " + currentScore + "/5");
+	theAccordion.accordion('option', {active: false});
 }
 
-function Button(_text, _color, _correctAnswer){
+function Button(_text, _color, _correctAnswer, _qNumber, _questionText){
 
 	this.EL = document.createElement("button");
 
 	this.color = _color || "#FFA500";
 	this.text = _text || "Master Button";
-	this.clickable = true;
 	this.correctAnswer = _correctAnswer;
-
-	//this.EL.onclick = function(){markAnswer(_correctAnswer)};
-
-
 
 	this.init = function(){
 		t = document.createTextNode(this.text);
 		this.EL.appendChild(t);
 
 		this.EL.addEventListener("click", function() {
-	    	markAnswer(this.correctAnswer);
+			var userAnswer = $("input:radio[name=question" +_qNumber + "]:checked").val();
+			if( typeof userAnswer != 'undefined'){
+	    		markAnswer(userAnswer,_correctAnswer, _qNumber, _questionText);
+	    	}
+	    	else{
+	    		alert("Please select answer!");
+	    	}
 		});
 
 		this.EL.style.backgroundColor = this.color;
